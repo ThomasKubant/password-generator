878f7322f0cbd5c056e061c46e35adbc944b5259
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-02-21 10:53:19 -0700

    added password length

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var lowerLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','
 var upperLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '.', '?', '`', '~'];
-
+var generatePassword = function() {
+  var password = "";
+  do {
+    var passwordLength = prompt("How many characters in your password?");
+    if(isNaN(passwordLength)) {
+      alert('Please enter a valid number.');
+    }
+  } while (isNaN(passwordLength));
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
